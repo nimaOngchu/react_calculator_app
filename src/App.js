325d640c28e16fdab1366484a9cf0d6240c155d5
nimaOngchu <nima.ongchu@gmail.com>
2019-05-05 23:56:44 +1000

    mathJs added

--- src/App.js
+++ src/App.js
@@ -1,117 +1,104 @@
-import './App.css';
-import React, { Component } from 'react';
-import Buttons from './Buttons'
+import "./App.css";
+import React, { Component } from "react";
+import Buttons from "./Buttons";
+import * as math from "mathjs";
 
 export class App extends Component {
   state = {
     data: [
-      {
-          value: 'ON',operator: true
-      }, {
-          value: 'BACK',operator: true
-      }, {
-          value: '%',operator: true
-      }, {
-          value: '/',operator:true
-      }, {
-          value: '7',operator: false
-      }, {
-          value: '8',operator: false
-      }, {
-          value: '9', operator: false
-      }, {
-          value: 'X', operator: true
-      }, {
-          value: '4',operator: false
-      }, {
-          value: '5',operator: false
-      }, {
-          value: '6',operator: false
-      }, {
-          value: '-',
-          operator: true
-      }, {
-          value: '1', operator: false
-      }, {
-          value: '2',operator: false
-      }, {
-          value: '3',operator: false
-      }, {
-          value: '+',operator: true
-      }, {
-          value: '0', operator: false
-      },
-      {
-          value: '.',operator: false
-      }, {
-          value: '=', operator: true
-      }],
-    total: '',
-    output:''
-  }
+      { value: "ON", operator: true },
+      { value: "BACK", operator: true },
+      { value: "%", operator: true },
+      { value: "/", operator: true },
+      { value: "7", operator: false },
+      { value: "8", operator: false },
+      { value: "9", operator: false },
+      { value: "X", operator: true },
+      { value: "4", operator: false },
+      { value: "5", operator: false },
+      { value: "6", operator: false },
+      { value: "-", operator: true },
+      { value: "1", operator: false },
+      { value: "2", operator: false },
+      { value: "3", operator: false },
+      { value: "+", operator: true },
+      { value: "0", operator: false },
+      { value: ".", operator: false },
+      { value: "=", operator: true }
+    ],
+    total: "0",
+    output: ""
+  };
   onButtonON() {
     this.setState({
-    output:''
-  })
+      output: "",
+      total:'0'
+    });
   }
 
-  onButtonClick(id, operator) {
-    this.setState({
-      total: this.state.output
-    })
-    let space = '';
-    let input = id;
-    if (operator) {
-      space = ' ';
-      if (id === 'BACK'|| id === 'ON'|| id === '=') {
-        // console.log('operator');
-        input = '';
-        space=''
-      }
+  calculate = (id, operator) => {
+    const notempty = this.state.output.length > 0;
+    if (id === "=") {
+      this.setState({
+        total:'= '+ math.eval(this.state.output)
 
+      });
+      console.log('calculating')
+    }
+  };
+  onButtonClick(id, operator) {
+    let space = "";
+    let input = id;
+    if (id === 'X') {
+      input = '*';
+    }
+
+    if (operator) {
+      space = " ";
+      if (id === '=')
+        input =''
     }
     this.setState({
-      output: this.state.output + space  + input + space
-    })
-    if (id === 'ON') {
+      output: this.state.output + space + input + space
+    });
+    if (id === "ON") {
       this.onButtonON();
     }
-    if (id === 'BACK') {
+    if (id === "BACK") {
+      console.log(this.state.output)
       this.setState({
         output: this.state.output.slice(0, -1)
-      })
+      });
+      console.log(this.state.output)
     }
-
+    this.calculate(id, operator);
   }
   componentDidMount() {
-    this.state.data.map((item) => {
+    this.state.data.map(item => {
       if (item.operator) {
-        document.getElementById(item.value).className = 'items operators';
-
+        document.getElementById(item.value).className = "items operators";
       }
       return null;
-    }
-
-    )
+    });
   }
 
   render() {
     return (
       <div className="App">
         <div className="container">
-        <span>Nima's calculator</span>
+          <span>Nima's calculator</span>
           <div className="output">
-
-            <span className='output_numbers' >{this.state.output}</span>
-            <span className ="output_total" >
-            {this.state.total}</span>
+            <span className="output_numbers">{this.state.output}</span>
+            <span className="output_total">{this.state.total}</span>
+          </div>
+          <Buttons
+            data={this.state.data}
+            onButtonClick={this.onButtonClick.bind(this)}
+          />
         </div>
-          < Buttons data={this.state.data} onButtonClick={this.onButtonClick.bind(this)}/>
       </div>
-
-    </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
