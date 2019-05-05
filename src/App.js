68b0b2afa44c04a5c9241b1238d340c782e17349
nimaOngchu <nima.ongchu@gmail.com>
2019-05-05 19:16:28 +1000

    buttons added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Buttons from './Buttons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export class App extends Component {
+  state = {
+    data: [
+      {
+          value: 'ON',operator: true
+      }, {
+          value: 'BACK',operator: true
+      }, {
+          value: '%',operator: true
+      }, {
+          value: '/',operator:true
+      }, {
+          value: '7',operator: false
+      }, {
+          value: '8',operator: false
+      }, {
+          value: '9', operator: false
+      }, {
+          value: 'X', operator: true
+      }, {
+          value: '4',operator: false
+      }, {
+          value: '5',operator: false
+      }, {
+          value: '6',operator: false
+      }, {
+          value: '-',
+          operator: true
+      }, {
+          value: '1', operator: false
+      }, {
+          value: '2',operator: false
+      }, {
+          value: '3',operator: false
+      }, {
+          value: '+',operator: true
+      }, {
+          value: '0', operator: false
+      },
+      {
+          value: '.',operator: false
+      }, {
+          value: '=', operator: true
+      }],
+    total: '',
+    output:''
+  }
+  onButtonON() {
+    this.setState({
+    output:''
+  })
+  }
+
+  onButtonClick(id, operator) {
+    this.setState({
+      total: this.state.output
+    })
+    let space = '';
+    let input = id;
+    if (operator) {
+      space = ' ';
+      if (id === 'BACK'|| id === 'ON'|| id === '=') {
+        // console.log('operator');
+        input = '';
+        space=''
+      }
+
+    }
+    this.setState({
+      output: this.state.output + space  + input + space
+    })
+    if (id === 'ON') {
+      this.onButtonON();
+    }
+    if (id === 'BACK') {
+      this.setState({
+        output: this.state.output.slice(0, -1)
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <div className="container">
+        <div className="output">
+            <span className='output_numbers' >{this.state.output}</span>
+            <span style={{
+              display: 'block',
+              padding: '5px',
+              marginTop: '25px'
+            }}>{this.state.total}</span>
+        </div>
+          < Buttons data={this.state.data} onButtonClick={this.onButtonClick.bind(this)}/>
+      </div>
+
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Buttons.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Buttons(props) {
+    var e = (id) => {
+        console.log(id);
+ }
+    return (
+
+      <div className="buttons">
+          {/* <div className="items operators">ON</div>
+          <div className="items operators">BACK</div>
+          <div className="items operators">%</div>
+          <div className="items operators">/</div>
+          <div className="items">7</div>
+          <div className="items">8</div>
+          <div className="items">9</div>
+          <div className="items operators">X</div>
+          <div className="items">4</div>
+          <div className="items">5</div>
+          <div className="items">6</div>
+          <div className="items operators">-</div>
+          <div className="items">1</div>
+          <div className="items">2</div>
+          <div className="items">3</div>
+          <div className="items operators">+</div>
+          <div className="items">0</div>
+          <div className="items">.</div>
+          <div className="items operators">=</div> */}
+                {props.data.map((item) =>
+                    (
+                        <div key={item.value} className="items" id={item.value} onClick = {() => props.onButtonClick(item.value, item.operator)}>{item.value}</div>
+
+                    ))}
+
+      </div>
+
+  )
+}
+
+export default Buttons
